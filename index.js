4078c5afb2e7d2dd1015f3515bc1c47025d24ce0
mdadeel <mdadeel125@gmail.com>
2026-01-07 18:50:33 +0600

    fixing

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
     origin: process.env.CLIENT_URL || '*', // Allow requests from deployed frontend
     credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Routes
 app.use('/api/products', productRoutes);
